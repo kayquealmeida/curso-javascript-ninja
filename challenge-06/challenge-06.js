1bf553c9e539912457a31fd9697e026f64c42335
kayquealmeida <38868120+kayquealmeida@users.noreply.github.com>
2019-11-17 20:14:46 -0300

    Update challenge-06.js

--- challenge-06/challenge-06.js
+++ challenge-06/challenge-06.js
@@ -4,7 +4,9 @@ para começar o desafio.
 Declare uma variável chamada `championship` que receberá o nome do campeonato,
 e imprima o nome desse campeonato no console.
 */
-// ?
+
+var championship = "Campeonato Brasileiro";
+console.log(championship); // Campeonato Brasileiro
 
 /*
 Declare uma variável chamada `teams`, que receberá um array com 5 elementos.
@@ -12,9 +14,12 @@ Os elementos serão nomes de times do campeonato escolhido, e os nomes devem
 estar na ordem em que eles aparecem na tabela no momento da solução desse
 desafio.
 */
-// ?
 
-console.log( 'Times que estão participando do campeonato:', teams );
+
+var teams = ["Flamengo","Palmeiras", "Santos", "Grêmio", "Atlético - PR"];
+
+console.log('Times que estão participando do campeonato: ', teams);
+
 
 /*
 Crie uma função chamada `showTeamPosition` com as seguintes características:
@@ -23,44 +28,100 @@ Crie uma função chamada `showTeamPosition` com as seguintes características:
     "O time que está em [POSIÇÃO]º lugar é o [NOME DO TIME].";
     - Onde [POSIÇÃO] é o valor passado por parâmetro e [NOME DO TIME] é o time
     que está nessa posição no array criado acima com os nomes dos times.
+    
     --------------
+
     Dica: lembre-se que arrays começam no índice zero, então a posição passada
     deve ser sempre um número a mais que o índice do array ;)
+    
     --------------
+    
     - A função só deve retornar a frase acima somente se o time estiver entre
     os 5 primeiros.
     - Se não houver time para a posição passada, deve retornar a mensagem:
     "Não temos a informação do time que está nessa posição."
 */
-// ?
+
+function showTeamPosition(numb) {
+    if(teams[numb - 1] === undefined){
+        return "Não temos a informação do time que está nessa posição.";
+    }
+
+    return "O time que está em " + numb + "º lugar é o " + teams[numb - 1];
+}
 
 /*
 Escolha 4 times do campeonato selecionado e mostre a posição dele, usando a
 função acima. Entre esses 4, adicione 1 que não esteja entre os 5 primeiros.
 */
-// ?
+
+console.log(showTeamPosition(1)); //O time que está em 1º lugar é o Flamengo
+console.log(showTeamPosition(3)); //O time que está em 3º lugar é o Santos
+console.log(showTeamPosition(4)); //O time que está em 4º lugar é o Grêmio
+console.log(showTeamPosition(5)); //O time que está em 5º lugar é o Atlético - PR
+console.log(showTeamPosition(6)); //Não temos a informação do time que está nessa posição.
 
 /*
 Mostre os números de 20 a 30 no console (inclusive o 30), usando a estrutura de
 repetição "while".
 */
-// ?
+
+var num = 20;
+
+while(num < 31){
+    console.log(num++);
+}
 
 /*
 Crie uma função chamada `convertToHex`, com as seguintes características:
+
     - A função recebe uma cor por parâmetro, do tipo string. Exemplo: "red";
+
     - Escolha 5 cores que serão convertidas do nome da cor para o seu
     equivalente hexadecimal (pode ser qualquer tom);
+
     - Usando a estrutura switch, verifique se a cor passada por parâmetro é
     algum hexa escolhido. Se for, retorne a frase:
     "O hexadecimal para a cor [COR] é [HEXADECIMAL].";
+
     - Se a cor passada por parâmetro não estiver entre as selecionadas, mostre
     a frase:
+
     "Não temos o equivalente hexadecimal para [COR]."
 */
-// ?
+
+function convertToHex(color) {
+    switch(color) {
+        case 'black':
+            return "O hexadecimal para a cor"+ color + " é #00000.";
+            break;       
+        case 'white':
+            return "O hexadecimal para a cor"+ color + " é #ffffff.";
+            break;        
+        case 'yellow':
+            return "O hexadecimal para a cor"+ color + " é #ffff00.";
+            break;        
+        case 'red':
+            return "O hexadecimal para a cor"+ color + " é #ff0000.";
+            break;        
+        case 'green':
+            return "O hexadecimal para a cor"+ color + " é #00ff00.";
+            break;
+        default:
+            return "Não temos o equivalente hexadecimal para "+ color +"."
+    }
+}
 
 /*
 Tente mostrar o hexadecimal de 8 cores diferentes usando a função criada acima.
 */
-// ?
+
+console.log(convertToHex('blue'));
+console.log(convertToHex('cyan'));
+console.log(convertToHex('magenta'));
+console.log(convertToHex('dark blue'));
+console.log(convertToHex('dark green'));
+console.log(convertToHex('light green'));
+console.log(convertToHex('green'));
+console.log(convertToHex('white'));
+console.log(convertToHex('black'));
